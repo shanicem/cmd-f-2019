6531d84340f05ce8d6dd1bb725e1713052c3755c
Shanice M <shanicemcsavaney@gmail.com>
2019-03-09 15:21:58 -0800

    Prettier

--- src/constants.js
+++ src/constants.js
@@ -1,18 +1,18 @@
 const offensiveWords = {
-    "bossy": [0, 0],
-    "guys": [1, 1],
-    "manpower": [1, 2],
-    "his": [1, 3]
+  bossy: [0, 0],
+  guys: [1, 1],
+  manpower: [1, 2],
+  his: [1, 3]
 };
 
 const explanations = [
-    "This term might be seen as offensive to women. Typically, this adheres to a stereotype used to describe women negatively.",
-    "This term uses gendered language that may insinuate that men are a preferred gender.",
+  "This term might be seen as offensive to women. Typically, this adheres to a stereotype used to describe women negatively.",
+  "This term uses gendered language that may insinuate that men are a preferred gender."
 ];
 
 const replacements = [
-    ["assertive", "strict"],
-    ["folks", "everyone", "team", "y'all"],
-    ["workforce", "workers", "team"],
-    ["their", "theirs", "one's"]
+  ["assertive", "strict"],
+  ["folks", "everyone", "team", "y'all"],
+  ["workforce", "workers", "team"],
+  ["their", "theirs", "one's"]
 ];
